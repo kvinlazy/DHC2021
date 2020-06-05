e7f81c8c43d74ce92fd0baa91efbffe68a4bd715
kvinlazy <kevinnaik@gmail.com>
2020-06-05 10:18:24 +0100

    upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slider').slider();
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    
+  });
